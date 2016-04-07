030815b336de4da6361fc17cde9833c2faeaffc7
Michelle <michelle.song@sjsu.edu>
2016-04-06 17:26:00 -0700

    Mergesort implemented

--- WordCount.java
+++ WordCount.java
@@ -1,13 +1,13 @@
 /**
  * CS 146 Section 6 
  * Data Structures and Algorithms
- * Project 3
+ * Project 4
  * 
  * Used and edited by: Michelle Lai, Michelle Song
  *
  * An executable that counts the words in a files and prints out the counts in
  * descending order or the number of unique elements in the file
- * Uses the data structure specified by the user
+ * Uses the data structure and sorting method specified by the user
  * */
 
 import java.io.IOException;
@@ -67,7 +67,7 @@ public class WordCount {
 	}
 
 	/**
-	 * Calculates the frequences for all the words in a given file
+	 * Calculates the frequencies for all the words in a given file
 	 * Uses file reader to cycle through all the words in the file and updates them into
 	 * the specified data structure by simply adding them or incrementing the count if already present
 	 * Prints the count and word for all the words in the file in descending order
@@ -128,12 +128,11 @@ public class WordCount {
 	}
 
 	/**
-	 * Sorts the given array in descending order by its count element
+	 * Sorts the given array in descending order by its count element with the specified sorting method
 	 * 
 	 * @param counts the array to be sorted
 	 */
-	private static <E extends Comparable<? super E>> void sortByDescendingCount(
-			DataCount<E>[] counts) {
+	private static <E extends Comparable<? super E>> void sortByDescendingCount(DataCount<E>[] counts) {
 
 		if (sort.compareTo("-is") == 0)
 		{
@@ -146,10 +145,15 @@ public class WordCount {
 		}
 		else if (sort.compareTo("-ms") == 0)
 		{
-			//			mergeSort(counts, 0, counts.length-1,
+			mergeSort(counts);
 		}
 	}
 
+	/**
+	 * Sorts the given array in descending order by its count element with insertion sort
+	 * 
+	 * @param counts the array to be sorted
+	 */
 	private static <E extends Comparable<? super E>> void insertionSort(DataCount<E>[] counts)
 	{
 		for (int i = 1; i < counts.length; i++) {
@@ -167,7 +171,14 @@ public class WordCount {
 			counts[j + 1] = x;
 		}
 	}
-
+	
+	/**
+	 * Sorts the given array in descending order by its count element with quick sort
+	 * 
+	 * @param counts the array to be sorted
+	 * @param low the lowest bound index to sort
+	 * @param high the highest bound index to sort
+	 */
 	private static <E extends Comparable<? super E>> void quickSort(DataCount<E>[] counts, int low, int high) 
 	{
 		int left = low;
@@ -208,36 +219,48 @@ public class WordCount {
 				getMedian[j + 1] = x;
 			}
 			// Pivot is the median
+
 			DataCount pivot = getMedian[1];
 			int pivotIndex = 0;
-			if (pivot == counts[mid]) pivotIndex = mid;
-			else if (pivot == counts[low]) pivotIndex = low;
-			else if (pivot == counts[high]) pivotIndex = high;
-			
+
+			// if all are the same, the first value is the median
+			if (getMedian[0].count == getMedian[1].count && getMedian[0].count == getMedian[2].count && getMedian[2].count == getMedian[1].count)
+			{
+				pivot = getMedian[0];
+				pivotIndex = low;
+			}
+			else
+			{
+				if (pivot == counts[mid]) pivotIndex = mid;
+				else if (pivot == counts[low]) pivotIndex = low;
+				else if (pivot == counts[high]) pivotIndex = high;
+			}
+
 			if (counts[pivotIndex].count != counts[low].count)
 			{
-			swap(counts, pivotIndex, low);
+				swap(counts, pivotIndex, low);
 			}
-			
+
 			left++;
 			while (left < right)
 			{
-				while ( counts[left].count >= pivot.count && left < high)
+				while ( counts[left].count >= pivot.count && (left < high))
 				{
 					left++;	
 				}
-				while (counts[right].count < pivot.count && right > low)
+				while (counts[right].count < pivot.count && (right > low))
 				{
 					right--;
 				}
-				if (left < right && counts[left].count != counts[right].count) 
+				if (left < right && (counts[left].count != counts[right].count)) 
 				{
 					swap(counts, right, left);
 				}
 			}
+
 			if (counts[low].count != counts[left-1].count)
 			{
-			swap(counts, low, left-1);
+				swap(counts, low, left-1);
 			}
 
 			pivotIndex = left-1;
@@ -254,6 +277,13 @@ public class WordCount {
 
 	}
 
+	/**
+	 * Swaps the given to elements around
+	 * 
+	 * @param counts the array in which the two elements will be switched
+	 * @param a the first element which needs to be swapped
+	 * @param b the second element which needs to be swapped
+	 */
 	public static <E> void swap (DataCount<E>[] counts, int a, int b)
 	{
 		DataCount temp = counts[a];
@@ -262,44 +292,104 @@ public class WordCount {
 
 	}
 
-	private static <E> void mergeSort(DataCount<E>[] dataCounts, Comparator<DataCount<E>> comparator) {
-		if(dataCounts.length > 1) {
-			int mid = dataCounts.length / 2;
-			DataCount<E>[] left = Arrays.copyOfRange(dataCounts, 0, mid);
-			DataCount<E>[] right = Arrays.copyOfRange(dataCounts, mid, dataCounts.length);
-
-			mergeSort(left, comparator);
-			mergeSort(right, comparator);
-			merge(dataCounts, left, right, comparator);
+	/**
+	 * Sorts the given array in descending order by its count element with quick sort
+	 * Splits the array into bounds
+	 * 
+	 * @param counts the array to be sorted
+	 */
+	private static <E extends Comparable<? super E>> void mergeSort(DataCount<E>[] counts)
+	{
+		int length = 1;
+		while (length <= counts.length)
+		{
+			int start = 0;
+			if ((length*2 > counts.length))
+			{
+				merge(start,start+length-1, counts.length-1, counts);
+			}
+			else
+			{
+				while (start+length < counts.length) 
+				{
+					merge(start, start+length-1, start+length+length-1, counts);
+					start = start + 2*length;			
+				}
+			}
+			length = length*2;
 		}
 	}
 
+	/**
+	 * Sorts between the given bounds and merges them back together after comparisons
+	 * 
+	 * @param counts the array to be sorted
+	 * @param low the lower bound of the segment that needs to be sorted
+	 * @param mid the middle index of the segment that needs to be sorted
+	 * @param high the upper bound of the segment that needs to be sorted
+	 */
+	private static <E extends Comparable<? super E>> void merge(int low, int mid, int high, DataCount<E>[] counts)
+	{  
+		if (high >= counts.length)
+		{
+			high = counts.length-1;
+		}
+		
+		DataCount<E>[] firstHalf = new DataCount[mid-low+1];
+		DataCount<E>[] secondHalf = new DataCount[high-mid];
+		int i = 0;
+		int j = 0;
+		
+		for (int p = low; p < mid+1; p++)
+		{
+			firstHalf[i] = counts[p];
+			i++;
+		}
+		for (int q = mid+1; q < high+1; q++)
+		{
+			secondHalf[j] = counts[q];
+			j++;
+		}
 
-	private static <E> void merge(DataCount<E>[] dataCounts,
-			DataCount<E>[] left, DataCount<E>[] right,
-			Comparator<DataCount<E>> comparator) {
-		int i = 0, j = 0;
-		for(int k = 0; k < dataCounts.length; k++) {
-			if(i < left.length && j < right.length) {
-				if(comparator.compare(left[i], right[j]) <= 0) {
-					dataCounts[k] = left[i++];
-				} else {
-					dataCounts[k] = right[j++];
-				}
-			} else if(i < left.length) {
-				dataCounts[k] = left[i++];
-			} else if(j < right.length) {
-				dataCounts[k] = right[j++];
+		int first = 0;
+		int second = 0;
+		int newValueIndex = low;
+		while (first < firstHalf.length && second < secondHalf.length)
+		{
+			if (firstHalf[first].count > secondHalf[second].count)
+			{
+				counts[newValueIndex] = firstHalf[first];
+				first++;
+				newValueIndex++;
 			}
+			else
+			{
+				counts[newValueIndex] = secondHalf[second];
+				second++;
+				newValueIndex++;
+			}
+		}
+
+		while (first < firstHalf.length) 
+		{ 
+			counts[newValueIndex] = firstHalf[first]; 
+			first++; 
+			newValueIndex++;
+		}
+		while (second < secondHalf.length) 
+		{ 
+			counts[newValueIndex] = secondHalf[second]; 
+			second++; 
+			newValueIndex++;
 		}
 	}
 
 
 	/**
-	 * The main class that takes in 3 user arguments, a specific data structure,
+	 * The main class that takes in 4 user arguments, a specific data structure, the desired sorting method,
 	 * the desired computation, and the file
-	 * The possible data structures are Binary Search Tree, -b, AVL tree, -a, and
-	 * Hash Table, -h
+	 * The possible data structures are Binary Search Tree, -b, AVL tree, -a, and Hash Table, -h
+	 * The possible sorting methods are Insertion Sort, -is, Quick Sort, -qs, and Merge Sort, -ms
 	 * The possible computations are the frequencies of all the elements, -frequency, and
 	 * the number of total unique words in the file, -num_unique
 	 * Prints return statement for the desired computation
@@ -310,14 +400,10 @@ public class WordCount {
 
 		if (args[2].equals("-frequency"))
 		{
-			//			System.out.println("Using frequency");
-			//			System.out.println("File is " + args[3]);
 			wc.countWords(args[3]);
 		}
 		else if (args[2].equals("-num_unique")) 
 		{
-			//			System.out.println("Using num_unique");
-			//			System.out.println("File is " + args[3]);
 			wc.numUnique(args[3]);
 		}
 	}
